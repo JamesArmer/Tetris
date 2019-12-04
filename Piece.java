c936b66fb68bf7cbb677f428e238340a4d624040
Mirka-S <mirkotimmer@live.nl>
2019-12-04 11:42:24 +0000

    Reviewed Errors

--- Piece.java
+++ Piece.java
@@ -1,33 +1,34 @@
-import blocks.*;
+import PieceOption;
 
 public class Piece {
     private int[] baseCoordinates;
     private int[][] shapeCoordinates;
     private int[] reomoved;
-    private Blocks shape;
+    private PieceOption shape;
     int size;
 
-    public Piece(int[] startCoord, Blocks type = blankPiece){
-        baseCoordinates = {0, 0};
+    public Piece(int[] startCoord, PieceOption type = blankPiece){
+        baseCoordinates = startCoord;
         shape = type;
         size = type.blockAmmount;
         removed = new int[];
         setShape(type);
     }
 
-    private void setShape(Blocks type){
+    private void setShape(PieceOption type){
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < 2; ++j) {
-                shapeCoordinates[i][j] = shape.coords[i][j];
+                shapeCoordinates[i][j] = type.relativeCoordinates[i][j];
             }
         }
     }
 
     public void removeAt(int[] coord){
-        int [] tempCoord
+        int [] tempCoord;
         for (int i = 0; i < size; i++) {
             if (coord == shapeCoordinates[i]){
-                shapeCoordinates[i] = {-1,- 1};
+                shapeCoordinates[i][0] = -1;
+                shapeCoordinates[i][1] = -1;
             }
         }
     }
