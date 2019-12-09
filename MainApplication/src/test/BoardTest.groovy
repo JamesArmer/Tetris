bf7859bf14d8e5e16706dbdd148687245e9c81c6
mikeynic <micjnic@icloud.com>
2019-12-09 11:54:11 +0000

    Created the test package with tests for the Tetris, Piece and Board classes.

---
+++ MainApplication/src/test/BoardTest.groovy
@@ -0,0 +1,23 @@
+package test
+
+class BoardTest extends GroovyTestCase {
+
+    void setUp() {
+        super.setUp()
+    }
+
+    void tearDown() {
+    }
+
+    void testActionPerformed() {
+    }
+
+    void testSquareWidth() {
+    }
+
+    void testSquareHeight() {
+    }
+
+    void testPaint() {
+    }
+}
---
+++ MainApplication/src/test/PieceTest.groovy
@@ -0,0 +1,25 @@
+package test
+
+class PieceTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+    }
+
+    void tearDown() {
+    }
+
+    void testRotateClock() {
+    }
+
+    void testRotateCounter() {
+    }
+
+    void testMinY() {
+    }
+
+    void testMinX() {
+    }
+
+    void testMaxX() {
+    }
+}
